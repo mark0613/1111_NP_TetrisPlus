11b26dffde37170548fd53e6359982f61e9e7d1e
mark0613 <goldffd700@gmail.com>
2022-11-27 22:02:37 +0800

    fix: unicode text

--- client/src/utils/file.py
+++ client/src/utils/file.py
@@ -5,10 +5,10 @@ import json
 def loadJsonFile(path):
     result = None
     if os.path.exists(path):
-        with open(path, "r") as file:
+        with open(path, "r", encoding="utf-8") as file:
             result = json.load(file)
     return result
 
 def dumpJsonFile(data, path):
-    with open(path, "w") as file:
-        json.dump(data, file)
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False)
